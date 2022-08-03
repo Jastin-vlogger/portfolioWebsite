69ab5e55cc14f92ca48b97e16cae1587c85ca0b9
Jastin-vlogger <justinkj765@gmail.com>
2022-08-03 10:38:41 +0530

    date upated

--- assets/js/main.js
+++ assets/js/main.js
@@ -227,3 +227,38 @@
   }
 
 })()
+
+
+// let date;
+// //Time
+// function startTime() {
+//   const today = new Date();
+//   let h = today.getHours();
+//   let m = today.getMinutes();
+//   let s = today.getSeconds();
+//   let onlyDate = today.getDate();
+//   let year = today.getFullYear();
+//   let month = today.getMonth() + 1;
+//   date = (onlyDate + "/" + month + "/" + year);
+//   m = checkTime(m);
+//   s = checkTime(s);
+//   if (h == 0) {
+//     h = 12
+//     session = "a.m.";
+//   }
+
+//   if (h > 12) {
+//     h = h - 12;
+//     session = "p.m.";
+//   }
+//   var Time = h + ":" + m + ":" + s + " " + session;
+//   setTimeout(startTime, 1000)
+//   return Time;
+// }
+
+// function checkTime(i) {
+//   if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+//   return i;
+// }
+
+// console.log(startTime());
